03814c451479d56fad12f405c46682e23fe4272f
Michael Anderson <aggie92@mindspring.com>
2026-05-22 07:06:08 -0500

    Initial Commit

---
+++ ServiceLink/ServiceLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceLinkApp.swift
+//  ServiceLink
+//
+//  Created by Michael Anderson on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ServiceLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
