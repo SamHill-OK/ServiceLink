36ce48c60514010575b957c2e7c73126539c338f
Michael Anderson <aggie92@mindspring.com>
2026-05-22 07:22:39 -0500

    Add initial ServiceLink folders and auth config

---
+++ ServiceLink/Auth/AuthModels.swift
@@ -0,0 +1,20 @@
+//
+//  AuthModels.swift
+//  ServiceLink
+//
+//  Created by Michael Anderson on 5/22/26.
+//
+
+import Foundation
+
+struct ServiceLinkSession: Codable {
+    let memberId: Int
+    let memberName: String
+
+    let clientId: Int
+    let clientName: String
+
+    let roleId: Int
+
+    let token: String
+}
---
+++ ServiceLink/Data/ApiConfig.swift
@@ -0,0 +1,13 @@
+//
+//  ApiConfig.swift
+//  ServiceLink
+//
+//  Created by Michael Anderson on 5/22/26.
+//
+
+import Foundation
+
+enum ApiConfig {
+    static let baseUrl =
+        "https://wsp-tools-dphadgczhbcnazab.centralus-01.azurewebsites.net"
+}
